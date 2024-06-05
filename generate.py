c1d7737b4cc41dbe4fdc9c654482b1add460a607
Jarno Tuovinen <spedepekka@gmail.com>
2024-06-05 10:36:00 +0300

    Plots relevantish data for a year for every hour

--- generate.py
+++ generate.py
@@ -1,11 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-cycles = 2 # how many sine cycles
-resolution = 25 # how many datapoints to generate
+# One year of temperatures is somewhat a single sive wave
+cycles = 1
+# How many measurements in a timeframe?
+# Once per hour for a year
+resolution = 365 * 24
+# From -1..1 to -30..30 which is quite good estimate for temperatures in Finland
+temp_amplitude = 30
 
 length = np.pi * 2 * cycles
-my_wave = np.sin(np.arange(0, length, length / resolution))
-plt.plot(my_wave)
-plt.show()
 
+number_of_points = length / resolution
+
+# Plain sine wave
+sine_wave = np.sin(np.arange(0, length, number_of_points))
+
+# Fix amplitude of the wave or the temperatures
+f = lambda x: x * temp_amplitude
+sine_wave = f(sine_wave)
+
+# Add a bit variation to measurements
+randoms = np.random.normal(0.0, 0.2, len(sine_wave))
+sine_wave = np.add(sine_wave, randoms)
+
+plt.plot(sine_wave)
+plt.show()
